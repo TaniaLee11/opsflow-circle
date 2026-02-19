eadefa57d3ec53601720172ab353a05e126c1e38
TaniaLee11 <248977229+TaniaLee11@users.noreply.github.com>
2026-02-19 11:39:21 -0500

    Revert "Add light/dark mode toggle to Hub"
    This reverts commit 2ff084f6a812f5971125e17ebc8dcf1da0735f0a.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { ThemeProvider } from "./contexts/ThemeContext.tsx";
 import "./index.css";
 import "./styles/glass-universal.css";
-import "./styles/theme.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
 // Force rebuild 1771518061
